fbda1bf167cac50661a04f0ef05bbe694942d640
shettynigamaraj <shettynigamaraj@gmail.com>
2026-07-29 18:39:26 +0530

    Create insertionatbegin.py

---
+++ insertionatbegin.py
@@ -0,0 +1,18 @@
+# Definition of singly linked list:
+# class ListNode:
+#     def __init__(self, x=0, next=None):
+#         self.data = x
+#         self.next = next
+
+class Solution:
+    def deleteHead(self, head):
+        #your code goes here
+        if head==None:
+            return None 
+        temp=head.next
+        head.next=None
+        head=temp
+        return head
+      '''Input: linkedList = [1, 2, 3], X = 7
+
+Output: [7, 1, 2, 3]'''
